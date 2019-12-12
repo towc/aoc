9f51a925e2ba1a71406beb70c1f525f49e9545c6
Matei Copot <matei@copot.eu>
2019-12-12 19:32:32 +0100

    day 12 part 2 submitted

--- day-12/sol.js
+++ day-12/sol.js
@@ -31,12 +31,12 @@ const test = (fn, samples) => {
 };
 // }}}
 
-const processInput = input => input.trim().split('\n').map(l => l.trim()).map(l => {
+const processInput = input => input.trim().split('\n').map(l => l.trim()).map((l) => {
   l = l.split('');
 
   // get rid of <>
   l.shift();
-  l.pop()
+  l.pop();
 
   l = l.join('');
 
@@ -44,48 +44,42 @@ const processInput = input => input.trim().split('\n').map(l => l.trim()).map(l 
   l.split(',').map(vs => vs.trim().split('=').map(p => p.trim())).forEach(([key, val]) => res[key] = Number(val));
 
   return res;
-})
+});
 
-const step = ms => {
-  for(let i = 0; i < ms.length - 1; ++i) {
+const stepAxis = (ms, axis) => {
+  for (let i = 0; i < ms.length - 1; ++i) {
     const am = ms[i];
 
-    for(let j = i + 1; j < ms.length; ++j) {
+    for (let j = i + 1; j < ms.length; ++j) {
       const bm = ms[j];
 
-      const x = am.x < bm.x ? 1 : -1;
-      const y = am.y < bm.y ? 1 : -1;
-      const z = am.z < bm.z ? 1 : -1;
+      const v = am[axis] < bm[axis] ? 1 : -1;
 
-      if (am.x !== bm.x) {
-        am.vx += x;
-        bm.vx -= x;
-      }
-
-      if (am.y !== bm.y) {
-        am.vy += y;
-        bm.vy -= y;
-      }
-
-      if (am.z !== bm.z) {
-        am.vz += z;
-        bm.vz -= z;
+      if (am[axis] !== bm[axis]) {
+        am[`v${axis}`] += v;
+        bm[`v${axis}`] -= v;
       }
     }
   }
 
-  for(const m of ms) {
-    m.x += m.vx;
-    m.y += m.vy;
-    m.z += m.vz;
+  for (const m of ms) {
+    m[axis] += m[`v${axis}`];
   }
 
   return ms;
-}
+};
 
-const energy = ms => {
+const step = (ms) => {
+  stepAxis(ms, 'x');
+  stepAxis(ms, 'y');
+  stepAxis(ms, 'z');
+
+  return ms;
+};
+
+const energy = (ms) => {
   let total = 0;
-  for(const m of ms) {
+  for (const m of ms) {
     const pot = Math.abs(m.x) + Math.abs(m.y) + Math.abs(m.z);
     const kin = Math.abs(m.vx) + Math.abs(m.vy) + Math.abs(m.vz);
 
@@ -93,68 +87,72 @@ const energy = ms => {
   }
 
   return total;
-}
+};
 
 const sol1 = (input, steps) => {
   const ms = processInput(input);
 
-  for(let i = 0; i < steps; ++i) {
+  for (let i = 0; i < steps; ++i) {
     step(ms);
   }
 
   return energy(ms);
 };
 
-const repeated = ms => {
-  for(const m of ms) {
-    if (m.vx !== 0 || m.vy !== 0 || m.vz !== 0) {
-      return false;
-    }
-  }
+const getPeriod = (ms, axis) => {
+  let count = 0;
+  const xs = ms.map(m => m[axis]);
+  const ixs = [...xs];
 
-  return true;
+  const vxs = ms.map(() => 0);
+
+  do {
+    ++count;
+    for(let i = 0; i < xs.length - 1; ++i) {
+      const ax = xs[i];
+      for(let j = i + 1; j < xs.length; ++j) {
+        const bx = xs[j];
+
+        const vx = ax < bx ? 1 : -1;
+
+        if (ax !== bx) {
+          vxs[i] += vx;
+          vxs[j] -= vx;
+        }
+      }
+    }
+
+    for(let i = 0; i < xs.length; ++i) {
+      xs[i] += vxs[i];
+    }
+  } while(xs.some((x, i) => ixs[i] !== x) || vxs.some(vx => vx !== 0));
+
+  return count;
 }
 
-const log = (i, ms) => {
-  let res = [];
-  for(const {x,y,z,vx,vy,vz} of ms) {
-    const p = [x, y, z].map(n => n.toString().padStart(3, ' '));
-    const v = [vx, vy, vz].map(n => n.toString().padStart(3, ' '));
-    res.push(p.join(' :') + '  @' + v.join(' :'));
+// simple lcm implementation, can take a few minutes
+const lcm = (...args) => {
+  const max = Math.max(...args);
+  args.splice(args.indexOf(max), 1);
+
+  let res = max;
+  while(args.some(x => (res % x) !== 0)) {
+    res += max;
   }
-  console.log(i, res.join(' ||| '));
+
+  return res;
 }
 
 const sol2 = (input) => {
   const ms = processInput(input);
-  const caches = ms.map(() => new Map());
 
-  let steps = 0;
-  
-  do {
-    ms.forEach((m, i) => {
-      const c = caches[i];
-      const s = [m.x, m.y, m.z, m.vx, m.vy, m.vz].map(n => (Math.sign(n) > 0 ? '+' : '-') + Math.abs(n)).join('')
+  const periods = {
+    x: getPeriod(ms, 'x'),
+    y: getPeriod(ms, 'y'),
+    z: getPeriod(ms, 'z'),
+  };
 
-      if (c.has(s)) {
-        console.log(steps, 'from ' + c.get(s) + ' already found state ' + s + ' at index ' + i);
-      } else {
-        c.set(s, steps);
-      }
-    })
-
-    step(ms);
-    ++steps;
-
-    //log(steps, ms);
-
-    //if (steps % 1000000 === 0) {
-    //  console.log(steps)
-    //}
-    //} while(!repeated(ms));
-  } while (steps <= 27000072)
-
-  return steps * 2;
+  return lcm(periods.x, periods.y, periods.z);
 };
 
 const samples1 = [
@@ -171,11 +169,11 @@ const samples1 = [
 
 ];
 const samples2 = [
-  //  [`
-  //<x=-1, y=0, z=2>  
-  //<x=2, y=-10, z=-7>
-  //<x=4, y=-8, z=8>  
-  //<x=3, y=5, z=-1>  `, 2772],
+  [`
+<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>  `, 2772],
   [`
 <x=-8, y=-10, z=0>
 <x=5, y=5, z=10>  
@@ -183,8 +181,8 @@ const samples2 = [
 <x=9, y=-8, z=-3> `, 4686774924],
 ];
 
-//test(sol1, samples1);
+// test(sol1, samples1);
 console.log(sol1(aocInput, 1000));
 
 //test(sol2, samples2);
-//console.log(sol2(aocInput))
+console.log(sol2(aocInput))
