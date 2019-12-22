8dbb1a28421f82e44c03da0c06233cf00bf74efe
Matei Copot <matei@copot.eu>
2019-12-22 08:02:13 +0100

    rb day 1 solved with testing setup

--- day-1/sol.rb
+++ day-1/sol.rb
@@ -1,4 +1,29 @@
+# setup {{{
 aoc_input = File.read('input')
+
+def test(fname, samples)
+  f = method(fname)
+  puts "TESTING #{fname}"
+  samples.each_with_index do |sample, i|
+    print " test #{i}: "
+    res = sample.last
+    r = f.call(*sample[0..-2])
+    if r != res
+      puts "FAIL "
+      puts "   expected"
+      puts "     #{res}"
+      puts "   but got"
+      puts "     #{r}"
+    else
+      puts "ok"
+    end
+  end
+end
+# }}}
+
+def process_input(input)
+  input.strip.split.map(&:strip) .map(&:to_i)
+end
 
 def fuel(n)
   (n / 3) - 2
@@ -6,21 +31,28 @@ end
 
 def fuelr(n)
   f = fuel(n)
-  if f <= 0
-    0
-  else
-    f + fuelr(f)
-  end
+  f <= 0 ? 0 : f + fuelr(f)
 end
 
 def sol1(input)
-  input.split.map(&:to_i).map {|n| fuel(n)} .sum
+  xs = process_input(input)
+  xs.map {|n| fuel(n)} .sum
 end
 
 def sol2(input)
-  input.split.map(&:to_i).map {|n| fuelr(n)} .sum
+  xs = process_input(input)
+  xs.map {|n| fuelr(n)} .sum
 end
 
+# def test(*_) return 'uncomment to disable tests'; end
 
-puts sol1 aoc_input
-puts sol2 aoc_input
+test :fuel, [
+  [12, 2],
+  [1969, 654],
+]
+p sol1 aoc_input
+test :fuelr, [
+  [1969, 966],
+  [100756, 50346],
+]
+p sol2 aoc_input
